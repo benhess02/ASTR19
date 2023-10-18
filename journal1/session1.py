70a1833632022da644961b2b886210afbd6dccdc
Ben Hess <pilotben@comcast.net>
2023-10-18 16:11:41 -0700

    Completed sessions 1-3.

---
+++ journal1/session1.py
@@ -0,0 +1,7 @@
+def main():
+    print("Ben Hess (he/him)")
+    print("My favorite movie is either The Martian or Ferris Bueller's Day Off.")
+    print("My favorite food is Mac 'n Cheese (especially when it's well seasoned and crispy on top)")
+
+if __name__ == "__main__":
+    main()
---
+++ journal1/session2.py
@@ -0,0 +1,7 @@
+def main():
+    print("2.5 + 3.6 = %s %s" % (2.5 + 3.6, type(2.5 + 3.6)))
+    print("5 - 3 = %s %s" % (5 - 3, type(5 - 3)))
+    print("8.2 * 6 = %s %s" % (8.2 * 6, type(8.2 * 6)))
+
+if __name__ == "__main__":
+    main()
